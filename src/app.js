84c61503ae305e06f430e3044485873796fc44d4
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-02-28 17:44:19 +0530

    added a new package.json and both server and client both run at same time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const path = require('path');
+
 const cors = require('cors');
 
 const planetRoutes =require("../src/routes/planets/planets.route");
@@ -7,10 +9,15 @@ const planetRoutes =require("../src/routes/planets/planets.route");
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3001"
+    origin: "http://localhost:3000"
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..','public')));
 app.use('/planets', planetRoutes);
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
+
 
 module.exports = app;
