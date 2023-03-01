f1b858e983c4ebf07066bf5c42124e26067f5d54
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-01 13:31:14 +0530

    added a new launch

--- src/models/launches.model.js
+++ src/models/launches.model.js
@@ -1,22 +1,37 @@
 const launches = new Map();
 
+const latestFlightNumber = 100;
 const launch = {
-    fligthNumber : 100,
+    fligthNumber: 100,
     mission: 'kepler exploration',
-    rocket:"Explore IS1",
+    rocket: "Explore IS1",
     launchDate: new Date('December 27,2025'),
-    destination:"kepler-442 b",
-    customer:["ZTM","NASA"],
-    upcoming:true,
-    success:true,
+    destination: "kepler-442 b",
+    customer: ["ZTM", "NASA"],
+    upcoming: true,
+    success: true,
 }
 
 launches.set(launch.fligthNumber, launch);
 
-function getAllLaunches(){
+function getAllLaunches() {
     return Array.from(launches.values())
+}
+
+function addNewLaunches(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign({
+            customer: ["Zero to Mastery", "NASA"],
+            upcoming: true,
+            success: true,
+            fligthNumber: latestFlightNumber,
+        })
+    )
 }
 
 module.exports = {
     getAllLaunches,
+    addNewLaunches,
 }
