cb493ad4a1ab86927a4c2439f0b7a71eb717d1a9
dpbhalani <dharmeshbhalani1712@gmail.com>
2023-03-01 14:35:57 +0530

    added a launch validation

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -9,9 +9,20 @@ function httpGetAllLaunches(req ,res){
 
 function htttpAddnewLaunch(req ,res){
     const launch = req.body;
+
+    if(!launch.mission || !launch.rocket || !launch.launchDate ||!launch.destination){
+        return res.status(400).json({
+            error:"misssing the your new launch detail..."
+        })
+    }
     
     launch.launchDate = new Date(launch.launchDate);
-
+    
+    if(isNaN(launch.launchDate)){
+        return res.status(400).json({
+            error:"Invalid the launch date ,please enter valid date.."
+        })
+    }
     addNewLaunch(launch);
     return res.status(201).json(launch);
 }
